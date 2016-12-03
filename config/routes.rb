b24aeb38431a0ed16e306718525ead7db1022796
jciancio <jciancio7@gmail.com>
2016-12-03 17:00:53 -0500

    stubbed out bill and votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  shallow do
+    resources :bills do
+      resources :votes, only: [:index, :create, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
